1d7b7c9a684c90a8278d5be8345c699f84b19259
Ivan_M <anaxynamoon022@gmail.com>
2025-06-13 00:13:06 +0300

    Add files via upload

---
+++ Import pygame, random, os, sys; Initialization pygame; Constants.py
@@ -0,0 +1,26 @@
+import pygame
+import random
+import os
+import sys
+from pygame.locals import *
+
+# Инициализация Pygame
+pygame.init()
+
+# Настройки по умолчанию
+DEFAULT_WIDTH = 1280
+DEFAULT_HEIGHT = 720
+FPS = 60
+
+# Цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+PINK = (255, 182, 193)
+GOLD = (255, 215, 0)
+DARK_GREEN = (0, 100, 0)
+DARK_BLUE = (0, 0, 100)
+DARK_RED = (100, 0, 0)
